e8b6974245b8a1da77b48d98088fe49dd035f195
Tedros Kahsu <tedkahcom@gmail.com>
2023-06-24 08:48:54 +0300

    Using JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <h1>Hello from the app component.</h1>;
+  const title = "Debdabe";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
+
+  return (
+    <div className="container">
+      <h1>Hello from the app component {title}</h1>
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
