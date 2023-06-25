da2b7ba7dce4e9f960e39a7b250c8f2970ebd49c
Tedros Kahsu <tedkahcom@gmail.com>
2023-06-25 04:54:16 +0300

    Component and conditional style

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import Card from "./shared/Card";
+import PropTypes from "prop-types";
 
 const FeedbackItem = ({ item }) => {
   return (
-    <div className="card" key={item.id}>
+    <Card reverse={true}>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
+};
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
 };
 
 export default FeedbackItem;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,8 @@
 import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
+
 const FeedbackList = ({ feedback }) => {
-  if (!feedback || feedback.length == 0) {
+  if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
   return (
@@ -10,6 +12,16 @@ const FeedbackList = ({ feedback }) => {
       ))}
     </div>
   );
+};
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default FeedbackList;
